661b61714d257966768a7a4937faab593f5ef4ba
Bobiptus <159236284+Bobiptus@users.noreply.github.com>
2025-03-21 23:20:10 -0700

    Create ley_ohm.py

---
+++ ley_ohm.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import random
+
+def calcular_resistencia():
+    v = random.randint(1, 220)
+    i = random.randint(1, 10)
+    r = v / i
+    mv = f"La corriente es {v} V\n"
+    mi = f"La corriente es {i} A\n"
+    mr = f"La corriente es {r:.2f} Ohm\n"
+    texto.insert(tk.END, mv)
+    texto.insert(tk.END, mi)
+    texto.insert(tk.END, mr)
+    return r
+
+def limpiar():
+    texto.delete("1.0", tk.END)
+
+
+root = tk.Tk()
+root.title("Ley de Ohm")
+root.geometry("300x150")
+
+marco = ttk.LabelFrame(root, text="Ley de Ohm")
+marco.pack()
+
+button = ttk.Button(root, text="Calcular resistencia", command=calcular_resistencia)
+button.pack()
+
+button_limpiar = ttk.Button(root, text="Limpiar", command=limpiar)
+button_limpiar.pack()
+
+texto = tk.Text(root, width=30, height=10)
+texto.pack()
+
+root.mainloop()
